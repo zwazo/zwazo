68e7ceff41cc4b71ea02d08246a85e699585f2bf
zwazo <zwazobleu@gmail.com>
2014-06-02 23:44:31 +0200

    3 univers 3 couleurs

---
+++ app/controller/Bookmarks.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\ControllerAbstract;
+
+class Bookmarks extends ControllerAbstract {
+
+	public function indexAction() {
+		
+		
+		
+	}
+
+}
---
+++ app/controller/Pro.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\ControllerAbstract;
+
+class Pro extends ControllerAbstract {
+
+	public function indexAction() {
+		
+		
+		
+	}
+
+}
