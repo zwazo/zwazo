e502cb0b5850921152f482d2977e7d137048fcbc
zwazo <zwazobleu@gmail.com>
2014-06-29 21:18:13 +0200

    home: perso/pro
    creation du module news (page par defaut de "perso")

---
+++ app/Controller/News.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\ControllerAbstract;
+
+/** 
+ * News Feed
+ */
+class News extends ControllerAbstract {
+
+	public function indexAction() {
+		
+		
+		
+	}
+
+}
