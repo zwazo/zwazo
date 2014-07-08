58991b562b90a5b8b862dc0cf86833f4031db3cb
zwazo <zwazobleu@gmail.com>
2014-07-08 23:43:51 +0200

    home
    home definitive

--- web/js/p_bookmarks.js
+++ web/js/p_bookmarks.js
@@ -1,21 +1,22 @@
 jQuery( document ).ready(function() {
 
-	jQuery( "#tag-autocomplete" ).autocomplete({ 
-		delay:500
-		,minLength:3
-		,source: function(request, response) {
-console.log('.source');
-			// jQuery( "#tag-autocomplete" ).autocomplete( "disable" );
-			// jQuery( "ul.ui-autocomplete" ).empty();
-			// TODO: inject spinner
-			
-			// ajax request
-			var ajaxresponse = ['java','foo','javascript'];
-			
-			// jQuery( "#tag-autocomplete" ).autocomplete( "enable" );
-			response( ajaxresponse );
-		}
-	});
-	
+	if ( jQuery( "#tag-autocomplete" ).attr('id') ) {
+		jQuery( "#tag-autocomplete" ).autocomplete({ 
+			delay:500
+			,minLength:3
+			,source: function(request, response) {
+	// console.log('.source');
+				// jQuery( "#tag-autocomplete" ).autocomplete( "disable" );
+				// jQuery( "ul.ui-autocomplete" ).empty();
+				// TODO: inject spinner
+				
+				// ajax request
+				var ajaxresponse = ['java','foo','javascript'];
+				
+				// jQuery( "#tag-autocomplete" ).autocomplete( "enable" );
+				response( ajaxresponse );
+			}
+		});
+	}
 	
 });
