b5d23820209bd9066c29370ad4024c8afd65ec0a
zwazo <zwazobleu@gmail.com>
2014-09-04 23:35:15 +0200

    Zwazo Bleu - from scratch

--- app/Controller/Home.php
+++ app/Controller/Home.php
@@ -10,17 +10,6 @@ class Home extends ControllerAbstract {
 
 	public function indexAction() {
 		
-		/*
-		$this->reset('stylesheet');
-		$this->stylesheet('unslider.css');
-		$this->stylesheet('p_home.css');
-		
-		$this->reset('script');
-		$this->script('unslider.min.js');
-		$this->script('p_home.js');
-		*/
-		
-		
 		$this->vars('layout', 'home.html.twig');
 		
 	}
--- web/js/p_home.js
+++
@@ -1,9 +0,0 @@
-function scrollto(selector) {
-	jQuery('html, body').animate({
-		scrollTop: jQuery( selector ).offset().top
-	}, 1000);
-}
-
-jQuery( document ).ready(function() {
-	
-});
